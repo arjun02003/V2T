1d1cce284ff903dd7d3ad0c16e9aa4f463d04464
Arjun <arjun@example.com>
2026-07-16 02:23:17 -0700

    Configure Firebase Realtime Database credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,10 @@
 // Firebase Connection Configuration
-// If left blank, the website will automatically fall back to browser-local IndexedDB.
 window.FIREBASE_CONFIG = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+  apiKey: "AIzaSyA4KyFi4S_0HJsperuMQru_SMg0Bu6JVVU",
+  authDomain: "tejas-9b892.firebaseapp.com",
+  databaseURL: "https://tejas-9b892-default-rtdb.firebaseio.com", // Default US region endpoint
+  projectId: "tejas-9b892",
+  storageBucket: "tejas-9b892.firebasestorage.app",
+  messagingSenderId: "945796715224",
+  appId: "1:945796715224:web:a48945ae045ffce23dbdad"
 };
